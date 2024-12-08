d4f2160d9193205d313e3ab94c470a909f34ce0c
2jh21 <127312344+2jh21@users.noreply.github.com>
2024-12-08 13:42:54 +0900

    Create qr_scanner_screen.dart

---
+++ flutter/lib/screens/qr_scanner_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QRScannerPage extends StatefulWidget {
+  @override
+  _QRScannerPageState createState() => _QRScannerPageState();
+}
+
+class _QRScannerPageState extends State<QRScannerPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR 코드 스캔'),
+      ),
+      body: QRView(
+        key: qrKey,
+        onQRViewCreated: _onQRViewCreated,
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      controller.pauseCamera(); // QR 스캔 멈춤
+      Navigator.pop(context, scanData.code); // 스캔된 데이터 반환
+    });
+  }
+}
